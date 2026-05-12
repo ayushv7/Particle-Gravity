66b297bd43fde9b5cf4a75c3985beab4f309bf90
ayushv7 <ayushvkulal2006@gmail.com>
2026-05-12 23:57:26 +0530

    js update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     const canvas = document.getElementById('gravityCanvas');
     const ctx = canvas.getContext('2d');
-
+    const width = canvas.width;
+    const height = canvas.height;
 }
